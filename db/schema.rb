15aed4ee779d7d1f59205e97768bf012b1c9ed3c
Klara Adams <klara.adams@mail.com>
2019-08-14 16:33:13 +0200

    simple airbnb

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_08_14_122455) do
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.text "description"
+    t.integer "price_per_night"
+    t.integer "number_of_guests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Studio Flat Paris',
+  address: 'Champs Elysee',
+  description: 'Central flat with 2 bathrooms and a spacious balcony. 10 min walk from the Eiffel Tower.',
+  price_per_night: 90,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Single Room in Berlin with Bathroom',
+  address: 'Rudi-Dutschke Strasse 16',
+  description: 'Room with ensuite bathroom in central Berlin with shared kitchen and parking spot. Free wifi.',
+  price_per_night: 52,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Brownstone on the Upper West Side',
+  address: '247 87th Street 10024 NY NY',
+  description: 'Spacious brownstone on the upper west side. 5 min walk to subway station & 10 min away from Central Park.',
+  price_per_night: 250,
+  number_of_guests: 8
+)
